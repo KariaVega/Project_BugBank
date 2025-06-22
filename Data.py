43c65d7869aaced6d3d125fccbe1e76bcc883fc6
Rosa Vega <arqkarvga@gmail.com>
2025-06-22 14:05:03 -0600

    Update Data.py
    minúsculas

--- Data.py
+++ Data.py
@@ -1,4 +1,4 @@
-Bug_Bank_url = 'https://bugbank.netlify.app/home'
+bug_bank_url = 'https://bugbank.netlify.app/home'
 email_field = 'katlin@gmail.com'
 name_field = 'Katherine Line'
 password_field = '1234567'
