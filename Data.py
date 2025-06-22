98de7a3a706172c915d0ae79b3faeb6756be6916
Rosa Vega <arqkarvga@gmail.com>
2025-06-22 14:06:26 -0600

    Update Data.py
    cambios en la url

--- Data.py
+++ Data.py
@@ -1,4 +1,4 @@
-bug_bank_url = 'https://bugbank.netlify.app/home'
+bug_bank_url = 'https://bugbank.netlify.app/'
 email_field = 'katlin@gmail.com'
 name_field = 'Katherine Line'
 password_field = '1234567'
